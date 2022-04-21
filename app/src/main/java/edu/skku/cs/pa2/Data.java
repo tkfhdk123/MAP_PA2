33d58a8cae13039c4a8f32c84985f7d0e8dc7c8d
Jay <tkfhdk123@gmail.com>
2022-04-22 01:26:47 +0900

    Add files

---
+++ app/src/main/java/edu/skku/cs/pa2/Data.java
@@ -0,0 +1,13 @@
+package edu.skku.cs.pa2;
+
+public class Data {
+    private boolean success;
+
+    public boolean isSuccess() {
+        return success;
+    }
+
+    public void setSuccess(boolean success) {
+        this.success = success;
+    }
+}
