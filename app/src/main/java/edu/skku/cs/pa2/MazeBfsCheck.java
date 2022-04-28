3f79322885b36a652a6f17b46b90c120846bea22
Jay <tkfhdk123@gmail.com>
2022-04-28 17:52:49 +0900

    fix imageview(user) size

--- app/src/main/java/edu/skku/cs/pa2/MazeBfsCheck.java
+++ app/src/main/java/edu/skku/cs/pa2/MazeBfsCheck.java
@@ -1,5 +1,16 @@
 package edu.skku.cs.pa2;
 
+import java.util.ArrayList;
+
 public class MazeBfsCheck {
+    private ArrayList<Cell> cells;
+    private Integer state;
+    private Integer maze_size;
+
+    public MazeBfsCheck(ArrayList<Cell> cells, Integer state, Integer maze_size){
+        this.cells = cells;
+        this.state = state;
+        this.maze_size = maze_size;
+    }
 
 }
